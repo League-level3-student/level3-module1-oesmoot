5045ceeadb347ae4d7d0a4320ad58cdefa13eda6
oesmo <oesmo@LaptopOfOwen>
2025-06-01 12:56:40 -0700

    done with guestbook

--- src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
+++ src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
@@ -10,7 +10,7 @@ public class _01_IntroToArrayLists {
         // 2. Add five Strings to your list
     	strings.add("string 1");
     	strings.add("string 2");
-    	strings.add("string 3");
+    	strings.add("string 3 e");
     	strings.add("string 4");
     	strings.add("string 5");
         // 3. Print all the Strings using a standard for-loop
@@ -23,11 +23,19 @@ public class _01_IntroToArrayLists {
     	}
         // 5. Print only the even numbered elements in the list.
     	for(int i = 0; i< strings.size(); i++) {
-    		if()
+    		if(i%2 == 0) {
+    			System.out.println(strings.get(i));
+    		}
     	}
         // 6. Print all the Strings in reverse order.
-
+    	for(int i = strings.size() -1; i>=0; i--) {
+    		System.out.println(strings.get(i));
+    	}
         // 7. Print only the Strings that have the letter 'e' in them.
-        
+        for (int i = 0; i< strings.size(); i++) {
+        	if(strings.get(i).contains("e")) {
+        		System.out.println(strings.get(i));
+        	}
+        }
     }
 }
--- src/_00_Intro_To_ArrayLists/_02_GuestBook.java
+++ src/_00_Intro_To_ArrayLists/_02_GuestBook.java
@@ -1,6 +1,15 @@
 package _00_Intro_To_ArrayLists;
 
-public class _02_GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_GuestBook implements ActionListener {
     /*
      * Create a GUI with two buttons. One button reads "Add Name" and the other
      * button reads "View Names". When the add name button is clicked, display
@@ -13,5 +22,37 @@ public class _02_GuestBook {
      * Guest #3: Greg Ganders
      * Guest #4: Donny Doners
      */
-
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JButton addName = new JButton();
+JButton view = new JButton();
+ArrayList<String> names = new ArrayList<String>();
+public void setup() {
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.add(addName);
+	panel.add(view);
+	view.setText("view names");
+	addName.setText("add name");
+	addName.addActionListener(this);
+	view.addActionListener(this);
+}
+public static void main(String[] args) {
+	_02_GuestBook book = new _02_GuestBook();
+	book.setup();
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	JButton buttonClicked = (JButton)e.getSource();
+	if(buttonClicked == addName) {
+		names.add(JOptionPane.showInputDialog("add a name to the guest book!"));
+	}
+	if(buttonClicked == view) {
+		for(int i =0; i< names.size(); i++) {
+			System.out.println("Guest #" + (i+1) +": " + names.get(i));
+		}
+	}
+}
 }
