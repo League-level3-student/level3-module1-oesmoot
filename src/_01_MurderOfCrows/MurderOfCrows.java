5d78aa258537f7fe52e067b67b08dfd7809a59c1
oesmo <oesmo@LaptopOfOwen>
2025-06-08 12:56:24 -0700

    done with murder of crows

--- src/_01_MurderOfCrows/MurderOfCrows.java
+++ src/_01_MurderOfCrows/MurderOfCrows.java
@@ -17,9 +17,14 @@ public class MurderOfCrows {
         // 1. One of the Crows has eaten the diamond. You need to search
         //    through the stomach of each Crow, then print the name of the
         //    guilty Crow.
-
+    	for(int i = 0; i< theMurder.size(); i++) {
+    		if(theMurder.get(i).getStomachContents().contains("diamond")) {
+    			System.out.println(theMurder.get(i).getName() + " has the diamond!");
+    			System.out.println((i+1) + " innocent crows had to die");
+    		}
+    	}
         // 2. How many innocent crows had to die before the diamond was found?
-
+    	
     }
 
     private void initializeCrows() {
